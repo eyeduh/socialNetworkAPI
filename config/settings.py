c4b9ac8f7be526ef4a24508e78690bdb21a294da
Aida Noori <ida@Aidas-MacBook-Pro.local>
2021-09-15 21:45:44 +0430

    Added 'media' dir

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = 'media/avatars'
+MEDIA_URL = '/media/avatars_urls'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
